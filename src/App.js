7a8abf2f50026b04b661e8d9368f58f6d7f387fc
ankit.s <ankit.s@ufaber.com>
2022-12-29 19:06:44 +0530

    today changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 import Expense from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expense />
     </div>
   );
