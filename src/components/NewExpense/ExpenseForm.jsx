6f400d342d5f8d8d47b1f054f05ec15b962847c9
ankit.s <ankit.s@ufaber.com>
2023-01-08 12:47:39 +0530

    today Changes

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -65,7 +65,8 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit" >Add Expense</button>
       </div>
     </form>
   );
